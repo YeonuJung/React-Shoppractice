60111561eea3c8c0348496dd54366270f0375c2c
YeonuJung <gsh95214@naver.com>
2024-02-23 17:50:41 +0900

    쇼핑몰 연습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Container, Navbar} from 'react-bootstrap'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar bg="light" data-bs-theme="light" className = "nav-bar">
+        <Container className = "container">
+          <Navbar.Brand href="#home">Food market</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Foods</Nav.Link>
+            <Nav.Link href="#pricing">about us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    <div className = "main-bg"></div>
+    <div className = "main-items">
+      <div className = "main-items-title">
+        <h3 style = {{fontWeight : "900"}}>지금 가장 많이 담는 특가😉</h3>
+        <p style = {{color : "gray"}}>Foodmarket 추천 특가템 최대 50%</p>
+      </div>
+      <div className = "items-container">
+      <div className = "item1"></div>
+      <div className = "item2"></div>
+      <div className = "item3"></div>
+      </div>
     </div>
+    </>
   );
 }
 
